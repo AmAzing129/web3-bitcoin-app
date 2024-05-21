a912a53670a28854e904329d73706ffdb31df890
AmAzing129 <web2crypto@proton.me>
2024-05-21 19:28:27 +0800

    feat: get inscriptions

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,23 @@
+"use client";
+
+import {
+  BitcoinWeb3ConfigProvider,
+  XverseWallet,
+  UnisatWallet,
+} from "@ant-design/web3-bitcoin";
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html>
+      <body>
+        <BitcoinWeb3ConfigProvider wallets={[XverseWallet(), UnisatWallet()]}>
+          {children}
+        </BitcoinWeb3ConfigProvider>
+      </body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,42 @@
 "use client";
 
-import { ConnectButton, Connector } from "@ant-design/web3";
-import {
-  BitcoinWeb3ConfigProvider,
-  XverseWallet,
-} from "@ant-design/web3-bitcoin";
+import { ConnectButton, Connector, NFTImage } from "@ant-design/web3";
+import { useBitcoinWallet } from "@ant-design/web3-bitcoin";
+import { useEffect, useState } from "react";
 
 const App: React.FC = () => {
+  const { provider, account, name } = useBitcoinWallet();
+  const [start, setStart] = useState(0);
+  const [size, setSize] = useState(10);
+  const [loading, setLoading] = useState(false);
+  const [inscriptions, setInscriptions] = useState([]);
+
+  useEffect(() => {
+    if (!account) return;
+    if (name === "Unisat Wallet") {
+      setLoading(true);
+      provider
+        .getInscriptions(start, size)
+        .then(({ list, total }) => {
+          setInscriptions(list);
+        })
+        .finally(() => {
+          setLoading(false);
+        });
+    }
+  }, [account, name, provider]);
+
   return (
-    <BitcoinWeb3ConfigProvider wallets={[XverseWallet()]}>
+    <>
       <Connector>
         <ConnectButton />
       </Connector>
-    </BitcoinWeb3ConfigProvider>
+      {inscriptions.length > 0
+        ? inscriptions.map(({ content, inscriptionNumber }) => (
+            <NFTImage width={300} tokenId={inscriptionNumber} src={content} />
+          ))
+        : null}
+    </>
   );
 };
 
